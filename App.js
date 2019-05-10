9b7ab2e7bdfcb0031ca0d31edf20f94b16c7888a
preithak10 <41484795+preithak10@users.noreply.github.com>
2019-05-10 17:04:16 +0530

    Update App.js

--- App.js
+++ App.js
@@ -13,34 +13,3 @@ export default class App extends Component {
     );
   }
 }
-
-//class using styles
-/*export default class App extends Component { 
-  render() {
-    return (
-      <View style={styles.container}>
-      <View style={styles.subcontainer}>
-        <Header />
-      </View>
-      </View>
-    );
-  }
-}*/
-
-//for style
-/*const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    //flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    backgroundColor: 'white'
-  },
-  subcontainer: {
-    flex: 0.1,
-    flexDirection: 'row',
-    // justifyContent: 'center',
-    // alignItems: 'flex-start',
-    
-  }
-});*/
