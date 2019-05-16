e4875afd951c5bd872d39045b653fde6c4dca990
preithak10 <41484795+preithak10@users.noreply.github.com>
2019-05-16 12:29:44 +0530

    Update AlbumList.js

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import { ScrollView } from 'react-native';
-//use axios to fetch json over http
-import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
+
+//use axios to fetch json over http
+/*import axios from 'axios';*/
 
 class AlbumList extends Component {
     state = { info: [] };
 
     
     //for json over http
-    componentWillMount() {
+    /*componentWillMount() {
         axios.get('https://api.myjson.com/bins/d1v9i')
         .then(response => this.setState({ info: response.data }));
+    }*/
+
+    //if you want to load json locally
+    componentWillMount() {
+        const customData = require('../json/museum.json');
+
+        this.setState({ info: customData.data });
     }
+
 
     renderItems() {
        return this.state.info.map(item => 
